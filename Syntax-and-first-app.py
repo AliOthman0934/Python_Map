2eb24d9108711ae4cda6488a8c2ad6fd880c5dc9
Ali Othman <alioth840@gmail.com>
2025-04-11 10:10:41 +0200

    Comments and how to use it

--- Syntax-and-first-app.py
+++ Syntax-and-first-app.py
@@ -1,1 +1,15 @@
-print("I LOVE PYTHON")
+print("I LOVE PYTHON") # Why i use the line
+# This is commit 
+
+
+#-------------------------------------------------------------
+# Infomation about the file
+#
+#
+#
+#
+#
+#
+#
+#
+#------------------------------------------------------------
