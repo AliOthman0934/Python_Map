7a576a5bd30ffef317f66ec9c98b1e90f16fd82f
Ali Othman <alioth840@gmail.com>
2025-04-13 10:06:11 +0200

    Concatenation

--- Syntax-and-first-app.py
+++ Syntax-and-first-app.py
@@ -18,3 +18,14 @@ print("I LOVE PYTHON") # Why i use the line
 name='This is variable'
 Phone= 25453232
 isMy= True
+person= {"name":"Ali","Age":30}
+li=(1,2,3,4,5)
+pi=34.50
+fruits=["appel","banana"]
+
+print(type(name))
+print(type(Phone))
+print(type(person))
+print(type(li))
+print(type(pi))
+print(type(fruits))
