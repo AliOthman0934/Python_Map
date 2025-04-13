c9b122326febb9a1cd64f7e933dde7d329f8612a
Ali Othman <alioth840@gmail.com>
2025-04-13 11:12:57 +0200

    Strings Methods part one

--- Strings-indexing-slicing.py
+++ Strings-indexing-slicing.py
@@ -1,5 +1,30 @@
-myString = "This is text"
+myString ="This is text"
 print(myString[0])
 print(myString[-1])
 print(myString[4:8])
 print(myString[4:8:2])
+
+# Strings emthods
+# strip(),rstrip(),lstrip()
+
+
+a ="   This is string    "
+print(a.strip())
+print(a.lstrip())
+
+#title()
+print(a.title())
+
+#capitalize()
+print(a.capitalize())
+
+b,d,f = "2","44","5953"
+print(b.zfill(4))
+print(d.zfill(4))
+print(f.zfill(4))
+
+#upper()
+print(a.upper())
+
+#lower()
+print(a.lower())
