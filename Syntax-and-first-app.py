9d49cbb049385e08bf5870dc98883a96f0000958
Ali Othman <alioth840@gmail.com>
2025-04-13 10:39:17 +0200

    Strings indexing and slicing

--- Syntax-and-first-app.py
+++ Syntax-and-first-app.py
@@ -21,11 +21,11 @@ isMy= True
 person= {"name":"Ali","Age":30}
 li=(1,2,3,4,5)
 pi=34.50
-fruits=["appel","banana"]
 
 print(type(name))
 print(type(Phone))
 print(type(person))
 print(type(li))
 print(type(pi))
-print(type(fruits))
+
+
