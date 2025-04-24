4f983a17f1d5bfda545686c32fc6d9f6d2dbbe93
Ali Othman <alioth840@gmail.com>
2025-04-24 10:57:07 +0200

    Set methods part one

--- Set.py
+++ Set.py
@@ -14,3 +14,48 @@ mySetThree={"Ali",343,"Osman",677,True,[1,2,3]}
 # Set itmes is unique
 mySetFour={1,2,"osama","osama",1}
 print(mySetFour) #it will not repate the same item tow time
+
+# Set methods part one
+
+# clear()
+a={1,2,3}
+a.clear()
+print(a)
+
+# union()
+b={"one","tow","three"}
+c={"1","2","3"}
+print(a | b)
+print(b.union(c))
+
+# add()
+d ={1,2,3,4}
+d.add(5)
+d.add(6)
+print(d)
+
+# copy()
+e={1,2,3,4}
+f=e.copy(e)
+print(f)
+
+# remove()
+g={2,3,5,6,874,35}
+g.remove(2)
+#g.remove(100) error
+
+# discard()
+h={55,38,1,4}
+h.discard(55)
+h.discard(100) # Does not show any error
+
+# pop()
+i={1,2,4,"ali",3}
+print(i.pop()) #It will pick rendom element form the set
+
+
+# update()
+j={3,4,5,"Ali"}
+k={3,4,5,"osman"}
+j.update(k)
+print(k)
