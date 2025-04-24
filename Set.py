2dcee2dcb6c3bc29aff2da145e4d7f7621f8dc5a
Ali Othman <alioth840@gmail.com>
2025-04-24 10:16:08 +0200

    Set in python

---
+++ Set.py
@@ -0,0 +1,16 @@
+# set in python
+# set itmes are inclosed in curly braces
+mySetOne={}
+
+# Set itmes are not ordered and not indexed
+mySetTow= {"Ali",23,True}
+print(mySetTow)
+#print(mySetTow[0]) not indexed
+
+# Set has only immutable data type(numbers,strings,tuples) list and dict not
+mySetThree={"Ali",343,"Osman",677,True,[1,2,3]}
+#print(mySetThree) error unhashable type list
+
+# Set itmes is unique
+mySetFour={1,2,"osama","osama",1}
+print(mySetFour) #it will not repate the same item tow time
